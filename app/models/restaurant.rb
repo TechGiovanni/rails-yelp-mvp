fe27fc705bef0cd6cfdc9b840fc7e89857f60078
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-11-09 11:58:05 -0500

    added a second model for the reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
+  category = %w[chinese italian japanese french belgian]
+
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: category, message: 'Please enter a valid size' }
 end
