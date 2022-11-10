f5ee2f890c014180d0e179d66b292fe98562d120
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-11-09 22:58:18 -0500

    Was ablt to tweek the code inorder to make the create verb work correctly

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,17 @@
 class ReviewsController < ApplicationController
-  before_action :set_restaurant, except: :destroy
-
-  def new
-    @review = Review.new
-  end
-
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
 
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: :unprocessable_entity
+      render 'restaurants/show', status: :unprocessable_entity
     end
   end
 
   private
-
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
-  end
 
   def review_params
     params.require(:review).permit(:rating, :content)
